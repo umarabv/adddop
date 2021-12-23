57bcf197fc8f11eec0cb524e045e69243f6caf03
umarabv <umar.abv0311@gmail.com>
2021-12-24 02:00:33 +0600

    asas

---
+++ app/src/main/java/com/example/dopppoints/SpidermanUnivers.kt
@@ -0,0 +1,11 @@
+package com.example.dopppoints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpidermanUnivers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spiderman_univers)
+    }
+}
---
+++ app/src/main/java/com/example/dopppoints/Spidermanback.kt
@@ -0,0 +1,11 @@
+package com.example.dopppoints
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Spidermanback : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spidermanback)
+    }
+}
